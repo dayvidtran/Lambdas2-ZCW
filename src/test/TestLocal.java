2bba0cfd3dff7ce8d0ad234f05d7ed0b75eedc10
dayvidtran <dayvidtran@gmail.com>
2023-04-20 17:10:06 -0400

    s

--- src/test/TestLocal.java
+++ src/test/TestLocal.java
@@ -1,10 +1,12 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class TestLocal {
 
     @Test
-    public void name(){
-        
+    public void testPersonChecker(){
+        ArrayList<Person> people = new ArrayList<>();
     }
 
 
