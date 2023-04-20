956ef6c11d388808dbb0e811276a0334c94557ce
dayvidtran <dayvidtran@gmail.com>
2023-04-20 16:38:18 -0400

    setup

---
+++ src/main/Person.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+public class Person {
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+
+    public int getAge() {
+        // ...
+        return 0;
+    }
+
+    public void printPerson() {
+        System.out.println(toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", gender=" + gender +
+                ", emailAddress='" + emailAddress + '\'' +
+                '}';
+    }
+}
+
